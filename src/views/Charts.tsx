0198531bfc7ca125c458d15b16fd236e22ab4294
Alexandre Freitas <alexandre.freitas@profranchising.com.br>
2024-05-08 19:37:32 -0300

    rendering one chart for gains and other for expenses

--- src/views/Charts.tsx
+++ src/views/Charts.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 import { collection, getDocs, query } from "firebase/firestore";
 import { Pie, PieChart, ResponsiveContainer } from "recharts";
 import { db } from "../firebase";
@@ -21,33 +21,63 @@ export const Charts = () => {
     const getUserHistory = async () => {
         const q = query(collection(db, "users", `${loggedUser?.email}`, "entries"));
         const querySnapshot = await getDocs(q);
-        const info: any = {};
+        const gains: any = {};
+        const expenses: any = {};
         querySnapshot.forEach((doc) => {
             const data = doc.data();
             const amount: number = data.amountInt + (data.amountDec / 100);
-            if (info[data.category])
-                info[data.category] += amount;
-            else
-                info[data.category] = amount;
+
+            if (data.type === "gain") {
+                if (gains[data.category])
+                    gains[data.category] += amount;
+                else
+                    gains[data.category] = amount;
+            } else {
+                if (expenses[data.category])
+                    expenses[data.category] += amount;
+                else
+                    expenses[data.category] = amount;
+            }
         });
 
-
-        return Object.keys(info).map(k => ({ category: k, value: info[k], fill: getRandomColor() }));
+        const result = {
+            expenses: Object.keys(expenses).map(k => ({ category: k, value: expenses[k], fill: getRandomColor() })),
+            gains: Object.keys(gains).map(k => ({ category: k, value: gains[k], fill: getRandomColor() })),
+        }
+        return result;
     }
 
     const { data } = useQuery({
-        queryKey: ['history'],
+        queryKey: ['charts'],
         queryFn: () => getUserHistory(),
         enabled: isLoggedIn && !!loggedUser.email?.length,
     })
 
+    let renderLabel = (entry: any) => {
+        return `${entry.category}: ${entry.value}`;
+    }
+
     return (
-        <Box>
-            <ResponsiveContainer aspect={1.2} maxHeight={500}>
-                <PieChart>
-                    <Pie data={data} dataKey="value" nameKey="category" innerRadius={60} outerRadius={80} label />
-                </PieChart>
-            </ResponsiveContainer>
+        <Box p={3}>
+            <Box>
+                <Heading as="h3" size="md">Ganhos:</Heading>
+                <ResponsiveContainer aspect={1.2} maxHeight={500}>
+                    <PieChart>
+                        <Pie isAnimationActive={false} data={data?.gains} dataKey="value" nameKey="category" innerRadius={60} outerRadius={80} label={renderLabel} />
+                    </PieChart>
+                </ResponsiveContainer>
+            </Box>
+            <Box>
+                <Heading as="h3" size="md">Gastos:</Heading>
+                <ResponsiveContainer aspect={1.2} maxHeight={500}>
+                    <PieChart>
+                        <Pie isAnimationActive={false}
+                            data={data?.expenses} dataKey="value"
+                            cx="50%" cy="50%"
+                            nameKey="category" innerRadius={60} outerRadius={80} label={renderLabel} />
+                    </PieChart>
+                </ResponsiveContainer>
+            </Box>
         </Box>
     )
 }
