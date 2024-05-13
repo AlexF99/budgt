bbeb44a64ff7232f808481ad14106cbb692e78af
Alexandre Freitas <alexandre.freitas@profranchising.com.br>
2024-05-13 18:32:42 -0300

    applying month filter

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,23 +1,71 @@
-import { Badge, Card, CardBody, Flex, Heading, IconButton, Spinner, Text, useDisclosure } from "@chakra-ui/react";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { Badge, Button, Card, CardBody, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, Flex, Heading, IconButton, Select, Spinner, Tag, Text, useDisclosure } from "@chakra-ui/react";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 import { db } from "../firebase";
 import { useQuery } from "@tanstack/react-query";
 import { useAuthStore } from "../zustand/authStore";
 import { Entry } from "../types/types";
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useRef, useState } from 'react';
 import _ from 'lodash';
 import { DeleteIcon } from "@chakra-ui/icons";
 import { DeleteDialog } from "../Components/DeleteDialog";
 import { parseMoney } from "../helpers/parseMoney";
+import { Controller, useForm } from "react-hook-form";
+import { Form } from "../Components/Form";
+import moment from "moment";
+
+type Filter = {
+    month: string,
+    dateRange: { dateFrom: Date, dateUntil: Date },
+}
 
 export const Home = () => {
+    // BEGIN FILTER
+    const btnRef = useRef(null);
+    const form = useForm<Filter>();
+    const { control, watch, setValue, reset } = form;
+
+    const [months, setMonths] = useState<any>({});
+
+    useEffect(() => {
+        const monthsOfYear = ["jan", "fev", "mar", "abril", "maio", "jun", "jul", "ago", "set", "out", "nov", "dez"]
+        const monthsAdd: any = {};
+        for (let i = 0; i < 5; i++) {
+            const current = moment().year(moment().year()).month(moment().month() - i)
+            const currentCopy = moment(current);
+            const label = monthsOfYear[current.get('month')] + current.get("year");
+            const monthAdd = {
+                start: current.date(current.startOf('month').date()).toDate(),
+                end: currentCopy.date(currentCopy.endOf('month').date()).toDate()
+            }
+            monthsAdd[label] = monthAdd;
+        }
+        setMonths({ ...months, ...monthsAdd })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const handleFilterSubmit = () => {
+        const m = form.getValues('month');
+        setValue('dateRange', { dateFrom: months[m]?.start, dateUntil: months[m]?.end })
+        onFilterClose();
+    }
+
+    const dateRange = watch('dateRange');
+    const month = watch('month');
+
+    // END FILTER
+
     const { isLoggedIn, loggedUser } = useAuthStore();
     const [entryId, setEntryId] = useState<string | undefined>(undefined);
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const { isOpen: isFilterOpen, onOpen: onFilterOpen, onClose: onFilterClose } = useDisclosure()
     const initial = { gains: 0, expenses: 0, total: 0 };
 
-    const getUserHistory = async () => {
-        const q = query(collection(db, "users", `${loggedUser?.email}`, "entries"), orderBy("createdAt", "desc"));
+    const getUserHistory = async (dateRange?: { dateFrom: Date, dateUntil: Date }) => {
+        const q = dateRange && dateRange.dateFrom && dateRange.dateUntil
+            ? query(collection(db, "users", `${loggedUser?.email}`, "entries"),
+                where("createdAt", "<=", dateRange.dateUntil),
+                where("createdAt", ">=", dateRange.dateFrom), orderBy("createdAt", "desc"))
+            : query(collection(db, "users", `${loggedUser?.email}`, "entries"), orderBy("createdAt", "desc"));
         const querySnapshot = await getDocs(q);
         const hist: any[] = []
         const info = { ...initial };
@@ -36,8 +84,8 @@ export const Home = () => {
     }
 
     const { data, isFetching } = useQuery({
-        queryKey: ['history'],
-        queryFn: () => getUserHistory(),
+        queryKey: ['history', dateRange],
+        queryFn: () => getUserHistory(dateRange),
         enabled: isLoggedIn && !!loggedUser.email?.length,
     })
 
@@ -51,6 +99,63 @@ export const Home = () => {
 
     return (
         <>
+            <Flex alignItems="center">
+                <Button ref={btnRef} colorScheme='teal' onClick={onFilterOpen}>
+                    Filtro
+                </Button>
+                {month &&
+                    <>
+                        <Tag size="sm" ml={4} borderRadius="full" colorScheme="gray">
+                            {month}
+                        </Tag>
+                        <Button variant="outline" size="sm" onClick={() => reset()}>
+                            Limpar filtro
+                        </Button>
+                    </>
+                }
+            </Flex>
+            <Drawer
+                isOpen={isFilterOpen}
+                placement='right'
+                onClose={onFilterClose}
+                finalFocusRef={btnRef}
+            >
+                <DrawerOverlay />
+                <DrawerContent>
+                    <DrawerCloseButton />
+                    <DrawerHeader>Selecione o período</DrawerHeader>
+
+                    <DrawerBody>
+                        <Form form={form}>
+                            <Controller
+                                control={control}
+                                name="month"
+                                render={({ field }) =>
+                                (<Select
+                                    name="month"
+                                    placeholder="Mês"
+                                    defaultValue="tudo"
+                                    onChange={(e) => {
+                                        e.preventDefault();
+                                        field.onChange(e);
+                                    }}
+                                >
+                                    <option value='all'>Tudo</option>
+                                    {Object.keys(months).map(m => (
+                                        <option key={m} value={m}>{m}</option>
+                                    ))}
+                                </Select>)} />
+                        </Form>
+                    </DrawerBody>
+
+                    <DrawerFooter>
+                        <Button variant='outline' mr={3} onClick={onFilterClose}>
+                            Cancelar
+                        </Button>
+                        <Button colorScheme='blue' onClick={handleFilterSubmit}>Aplicar</Button>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
             <Heading as="h3" size="md">Resumo:</Heading>
             <Card mt={3}>
                 <CardBody>
