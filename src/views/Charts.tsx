9616cf38be4493ac438f7548ee08429bbbde4df5
Alexandre Freitas <alexandre.freitas@profranchising.com.br>
2024-06-27 18:04:06 -0300

    added filter to charts component

--- src/views/Charts.tsx
+++ src/views/Charts.tsx
@@ -1,13 +1,20 @@
-import { Box, Flex, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
-import { collection, getDocs, query } from "firebase/firestore";
+import { Box, Button, Flex, Heading, IconButton, SimpleGrid, Spinner, Tag, useDisclosure } from "@chakra-ui/react";
+import { QueryFieldFilterConstraint, collection, getDocs, query, where } from "firebase/firestore";
 import { Bar, BarChart, CartesianGrid, Legend, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 import { db } from "../firebase";
 import { useAuthStore } from "../zustand/authStore";
 import { useQuery } from "@tanstack/react-query";
+import { Filter } from "../types/types";
+import { useFilter } from "../hooks/useFilter";
+import { FilterDrawer } from "../Components/FilterDrawer";
+import { useRef } from "react";
+import { CloseIcon } from "@chakra-ui/icons";
 
 export const Charts = () => {
-
+    const openFilterBtnRef = useRef(null);
+    const { isOpen: isFilterOpen, onOpen: onFilterOpen, onClose: onFilterClose } = useDisclosure();
     const { isLoggedIn, loggedUser } = useAuthStore();
+    const { isFilterActive, filterData, setFilterData } = useFilter();
 
     const getRandomColor = () => {
         var letters = '0123456789ABCDEF';
@@ -18,8 +25,12 @@ export const Charts = () => {
         return color;
     }
 
-    const getUserHistory = async () => {
-        const q = query(collection(db, "users", `${loggedUser?.email}`, "entries"));
+    const getUserHistory = async (filterData: Filter) => {
+        const { dateFrom, dateUntil } = filterData;
+        const whereClauses: QueryFieldFilterConstraint[] = dateFrom && dateUntil ?
+            [where("createdAt", "<=", dateUntil), where("createdAt", ">=", dateFrom)] : [];
+        const q = query(collection(db, "users", `${loggedUser?.email}`, "entries"), ...whereClauses);
+
         const querySnapshot = await getDocs(q);
         const gains: any = {};
         const expenses: any = {};
@@ -52,8 +63,8 @@ export const Charts = () => {
     }
 
     const { data, isFetching } = useQuery({
-        queryKey: ['charts'],
-        queryFn: () => getUserHistory(),
+        queryKey: ['charts', filterData],
+        queryFn: () => getUserHistory(filterData),
         enabled: isLoggedIn && !!loggedUser.email?.length,
     })
 
@@ -71,9 +82,38 @@ export const Charts = () => {
 
     return (
         <Box>
+            <FilterDrawer isFilterOpen={isFilterOpen}
+                onFilterClose={onFilterClose}
+                btnRef={openFilterBtnRef}
+                setFilterData={setFilterData}
+                filterData={filterData}
+            />
             <Box>
+                <Flex alignItems="center" justify="end" w="full">
+                    {isFilterActive &&
+                        <Flex>
+                            <Tag size="lg" ml={4} colorScheme="gray">
+                                {filterData.month}
+                            </Tag>
+                            <IconButton
+                                size="sm"
+                                onClick={() => setFilterData({})}
+                                icon={<CloseIcon w={3} h={3} />}
+                                aria-label={'Toggle Navigation'}
+                            />
+                        </Flex>
+                    }
+                    <Button ref={openFilterBtnRef}
+                        colorScheme='teal'
+                        size="sm"
+                        ml={1}
+                        onClick={onFilterOpen}
+                        variant={isFilterActive ? 'solid' : 'outline'}>
+                        Filtro
+                    </Button>
+                </Flex>
                 <Heading as="h3" size="md">Ganhos vs Gastos:</Heading>
-                <ResponsiveContainer aspect={1} maxHeight={200}>
+                <ResponsiveContainer aspect={1} maxHeight={300}>
                     <BarChart width={200} height={100} data={data?.comparison}>
                         <CartesianGrid strokeDasharray="3 3" />
                         <XAxis dataKey="name" />
